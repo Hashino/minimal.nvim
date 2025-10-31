b4a23fe2f83e2c56d0086cc149052f84573fa93a
Hashino <iam.ravara@gmail.com>
2025-10-31 14:54:30 -0300

    removed utility plugins

--- init.lua
+++ init.lua
@@ -247,18 +247,5 @@ require("which-key").setup({
   }
 })
 
--- INFO: utility plugins
-vim.pack.add({
-  "https://github.com/windwp/nvim-autopairs",    -- auto pairs
-  "https://github.com/VidocqH/auto-indent.nvim", -- auto indent
-  "https://github.com/numToStr/Comment.nvim",    -- gb/gc to (un)comment lines
-  "https://github.com/folke/todo-comments.nvim"  -- highlight TODO/INFO/WARN comments
-}, { confirm = false })
-
-require("nvim-autopairs").setup()
-require("auto-indent").setup()
-require("Comment").setup()
-require("todo-comments").setup()
-
 -- uncomment to enable automatic plugin updates
 -- vim.pack.update()
