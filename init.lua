a1f0c813bc8f5a8fb7b8e56b9a2fb9bc62c5c8ee
Hashino <iam.ravara@gmail.com>
2025-11-01 20:23:39 -0300

    feat(blink.cmp): added keybinds for Tab/Enter usage

--- init.lua
+++ init.lua
@@ -192,17 +192,18 @@ require("blink.cmp").setup({
     },
   },
 
-  -- default blink keymaps
   keymap = {
-    ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
     ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
-
+    ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
     ['<C-y>'] = { 'select_and_accept', 'fallback' },
     ['<C-e>'] = { 'cancel', 'fallback' },
-    ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
 
-    ['<Tab>'] = { 'snippet_forward', 'fallback' },
-    ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+    ['<Tab>'] = { 'snippet_forward', 'select_next', 'fallback' },
+    ['<S-Tab>'] = { 'snippet_backward', 'select_prev', 'fallback' },
+    ['<CR>'] = { 'select_and_accept', 'fallback' },
+    ['<Esc>'] = { 'cancel', 'hide_documentation', 'fallback' },
+
+    ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
 
     ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
     ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
