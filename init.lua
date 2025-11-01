82542ef877476c452c5b3524a38409e6636845a8
Hashino <iam.ravara@gmail.com>
2025-11-01 20:48:29 -0300

    added some sane default options

--- init.lua
+++ init.lua
@@ -1,3 +1,16 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.signcolumn = "yes"
+
+vim.opt.breakindent = true
+vim.opt.wrap = true
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.textwidth = 80
+
 vim.diagnostic.config({ virtual_text = true })
 
 vim.pack.add({
