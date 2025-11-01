be014640e7c3dafbc1b71e28ea24b58afbe4f86d
Hashino <iam.ravara@gmail.com>
2025-11-01 12:30:41 -0300

    docs(init.lua): uncapitalized comments
    for standardization

--- init.lua
+++ init.lua
@@ -1,68 +1,68 @@
 -- INFO: options
 
--- Set <space> as the leader key
--- Must happen before plugins are loaded (otherwise wrong leader will be used)
+-- set <space> as the leader key
+-- must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- Enable true color support
+-- enable true color support
 vim.opt.termguicolors = true
 
--- Make line numbers default
+-- make line numbers default
 vim.opt.number = true
 vim.opt.relativenumber = true
 
--- Enable mouse mode, can be useful for resizing splits for example!
+-- enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = "a"
 
--- Don't show the mode, since it's already in the status line
+-- don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
+-- sync clipboard between OS and Neovim.
+--  remove this option if you want your OS clipboard to remain independent.
+--  see `:help 'clipboard'`
 vim.opt.clipboard = "unnamedplus"
 
--- Enable break indent
+-- enable break indent
 vim.opt.breakindent = true
 
--- Save undo history
+-- save undo history
 vim.opt.undofile = true
 
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+-- case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
--- Keep signcolumn on by default
+-- keep signcolumn on by default
 vim.opt.signcolumn = "yes"
 
--- Decrease update time
+-- decrease update time
 vim.opt.updatetime = 250
 
--- Decrease mapped sequence wait time
--- Displays which-key popup sooner
+-- decrease mapped sequence wait time
+-- displays which-key popup sooner
 vim.opt.timeoutlen = 300
 
--- Configure how new splits should be opened
+-- configure how new splits should be opened
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
--- Sets how neovim will display certain whitespace characters in the editor.
+-- sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 vim.opt.list = true
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣", }
 
--- Preview substitutions live, as you type!
+-- preview substitutions live, as you type!
 vim.opt.inccommand = "split"
 
--- Show which line your cursor is on
+-- show which line your cursor is on
 vim.opt.cursorline = true
 
--- Set highlight on search, but clear on pressing <Esc> in normal mode
+-- set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
 
--- Enable line wrapping
+-- enable line wrapping
 vim.opt.wrap = true
 
 -- formatting
