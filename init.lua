845babdc9ca539e2b81c7e4c453acac09dcab036
Hashino <iam.ravara@gmail.com>
2025-11-05 14:20:12 -0300

    compacted code

--- init.lua
+++ init.lua
@@ -1,8 +1,6 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
-
 vim.opt.signcolumn = "yes"
-
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
@@ -16,18 +14,15 @@ vim.pack.add({
   "https://github.com/neovim/nvim-lspconfig",
   "https://github.com/mason-org/mason.nvim",
   "https://github.com/mason-org/mason-lspconfig.nvim",
-  "github.com/WhoIsSethDaniel/mason-tool-installer.nvim",
+  "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim",
 }, { confirm = false })
 
 require("nvim-treesitter.install").update("all")
 require("nvim-treesitter.configs").setup({ auto_install = true })
-
 require("blink.cmp").setup({ fuzzy = { implementation = "lua" } })
 
 local lsp_servers = {
-  lua_ls = {
-    Lua = { workspace = { library = vim.api.nvim_get_runtime_file("lua", true) } },
-  },
+  lua_ls = { Lua = { workspace = { library = vim.api.nvim_get_runtime_file("lua", true) } }, },
 }
 
 require("mason").setup()
